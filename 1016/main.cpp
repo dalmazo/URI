f432afd48698735714fd58a67892d24ec4d93187
dalmazo <dalmazo.rafael@gmail.com>
2019-07-28 22:46:04 -0300

    1016, 1017 added

---
+++ 1016/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <stdlib.h>
+#include <math.h>
+ 
+using namespace std;
+ 
+int main() {
+     
+     int x;
+     
+     cin >> x;
+     
+     printf("%i minutos\n", x*2);
+ 
+    return 0;
+}
---
+++ 1017/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <stdlib.h>
+#include <math.h>
+ 
+using namespace std;
+ 
+int main() {
+     
+     int A, B;
+     
+     cin >> A;
+     cin >> B;
+     
+     printf("%.3f\n", (A*B)/12.0);
+ 
+    return 0;
+}
